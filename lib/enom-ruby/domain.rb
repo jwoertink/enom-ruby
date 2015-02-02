59e1293bf034204702ec3053dcba83e587e4126c
Jeremy Woertink <jeremywoertink@gmail.com>
2015-02-02 09:13:33 -0800

    new check method for domain

--- lib/enom-ruby/domain.rb
+++ lib/enom-ruby/domain.rb
@@ -1,10 +1,15 @@
+require 'hashie/mash'
+
 module EnomRuby
 
   class Domain
     DEFAULT_TLDS = ['com']
 
     def self.check(sld, tld_list = DEFAULT_TLDS)
-      Client.request(command: 'Check', sld: sld, tldlist: tld_list.join(','))
+      @response = Client.request(command: 'Check', sld: sld, tldlist: tld_list.join(','))
+      clean_hash = @response['interface_response'].inject({}) {|hsh, (k,v)| hsh[k.downcase] = v; hsh }
+      clean_hash['results'] = Hash[*clean_hash['domain'].zip(clean_hash['rrptext'])]
+      Hashie::Mash.new(clean_hash)
     end
   end
 end
