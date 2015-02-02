b1622757c940dc62ed42273a1f07f0385e3eb486
Jeremy Woertink <jeremywoertink@gmail.com>
2015-02-02 09:13:51 -0800

    cleaning up the client object

--- lib/enom-ruby/client.rb
+++ lib/enom-ruby/client.rb
@@ -15,22 +15,22 @@ module EnomRuby
 
       # All requests must contain the UID, PW, and ResponseType query parameters
       def default_params
-        { "UID" => self.username, "PW" => self.password, "ResponseType" => "xml"}
+        { "UID" => self.username, "PW" => self.password, "ResponseType" => "xml" }
       end
 
       # Enom has a test platform and a production platform.  Both are configured to use
-      # HTTPS at all times. Don"t forget to configure permitted IPs (in both environments)
-      # or you"ll get InterfaceErrors.
+      # HTTPS at all times. Don't forget to configure permitted IPs (in both environments)
+      # or you'll get InterfaceErrors.
       def base_uri
-        @base_uri = test? ? "https://resellertest.enom.com/interface.asp" : "https://reseller.enom.com/interface.asp"
+        subdomain = test? ? 'resellertest' : 'reseller'
+        "https://#{subdomain}.enom.com/interface.asp"
       end
 
-      # All requests to Enom are GET requests, even when we"re changing data.  Unfortunately,
+      # All requests to Enom are GET requests, even when we're changing data.  Unfortunately,
       # Enom also does not provide HTTP status codes to alert for authentication failures
       # or other helpful statuses -- everything comes back as a 200.
       def request(params = {})
-        params.merge!(default_params)
-        response = get(base_uri, :query => params)
+        response = get(base_uri, query: params.merge(default_params))
         case response.code
         when 200
           if response["interface_response"]["ErrCount"] == "0"
