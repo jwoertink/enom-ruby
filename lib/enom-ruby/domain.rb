15c361890d8634ece022ce8da5607be5d70fa47c
Jeremy Woertink <jeremywoertink@gmail.com>
2015-02-13 11:48:36 -0800

    ensure tld list is an array

--- lib/enom-ruby/domain.rb
+++ lib/enom-ruby/domain.rb
@@ -7,7 +7,7 @@ module EnomRuby
 
     # Check a particular domain (sld) with an array of tlds (tld_list)
     def self.check(sld, tld_list = DEFAULT_TLDS)
-      response = Client.request(command: 'Check', sld: sld, tldlist: tld_list.join(','))
+      response = Client.request(command: 'Check', sld: sld, tldlist: Array(tld_list).join(','))
       domains = Hash[*Array(response['Domain']).zip(Array(response['RRPText'])).flatten]
       [].tap do |results|
         domains.each_pair { |domain, text| results << new(domain, text) }
